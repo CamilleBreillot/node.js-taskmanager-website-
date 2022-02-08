88810029368e010d363bb3be7711e7123077a320
camille breillot <camille.breillot@gmail.com>
2022-02-08 15:47:21 +0100

    set env variable for dev with env-cmd package

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,6 @@
 const sgMail = require('@sendgrid/mail')
-const sendgridAPIkey = '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'
 
-sgMail.setApiKey(sendgridAPIkey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
   sgMail.send( {
