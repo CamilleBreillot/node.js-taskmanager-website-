a0cbe1a0065f02908482f68b320714033e4d0057
camille breillot <camille.breillot@gmail.com>
2022-02-09 12:25:25 +0100

    user tests implemeted with db test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT // we remove || 3000 thanks to setup env var with npm emv cmd & integration in script
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
